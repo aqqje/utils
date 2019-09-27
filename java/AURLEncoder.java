ad3cdac874281ddbfee0f1f8b0091add2736019b
aqqje <1042136232@qq.com>
2019-09-27 15:21:18 +0800

    调用支付宝扫一扫

---
+++ java/AURLEncoder.java
@@ -0,0 +1,114 @@
+package com.pepay.app.commons.utils;
+
+import sun.security.action.GetPropertyAction;
+
+import java.io.CharArrayWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.security.AccessController;
+import java.util.BitSet;
+
+/**
+ * URL 编码工具类
+ * Created by aqqje on 2019/9/18 0018
+ */
+public class AURLEncoder {
+    static BitSet dontNeedEncoding;
+    static final int caseDiff = ('a' - 'A');
+    static String dfltEncName = null;
+
+    static {
+
+        dontNeedEncoding = new BitSet(256);
+        int i;
+        dfltEncName = AccessController.doPrivileged(
+                new GetPropertyAction("file.encoding")
+        );
+    }
+
+
+    private AURLEncoder() { }
+
+    public static String encode(String s) {
+
+        String str = null;
+
+        try {
+            str = encode(s, dfltEncName);
+        } catch (UnsupportedEncodingException e) {
+        }
+
+        return str;
+    }
+
+    public static String encode(String s, String enc)
+            throws UnsupportedEncodingException {
+
+        boolean needToChange = false;
+        StringBuffer out = new StringBuffer(s.length());
+        Charset charset;
+        CharArrayWriter charArrayWriter = new CharArrayWriter();
+
+        if (enc == null)
+            throw new NullPointerException("charsetName");
+
+        try {
+            charset = Charset.forName(enc);
+        } catch (IllegalCharsetNameException e) {
+            throw new UnsupportedEncodingException(enc);
+        } catch (UnsupportedCharsetException e) {
+            throw new UnsupportedEncodingException(enc);
+        }
+        for (int i = 0; i < s.length();) {
+            int c = (int) s.charAt(i);
+            //System.out.println("Examining character: " + c);
+            if (dontNeedEncoding.get(c)) {
+                if (c == ' ') {
+                    c = '+';
+                    needToChange = true;
+                }
+                out.append((char)c);
+                i++;
+            } else {
+                do {
+                    charArrayWriter.write(c);
+                    if (c >= 0xD800 && c <= 0xDBFF) {
+                        /*
+                          System.out.println(Integer.toHexString(c)
+                          + " is high surrogate");
+                        */
+                        if ( (i+1) < s.length()) {
+                            int d = (int) s.charAt(i+1);
+                            if (d >= 0xDC00 && d <= 0xDFFF) {
+                                charArrayWriter.write(d);
+                                i++;
+                            }
+                        }
+                    }
+                    i++;
+                } while (i < s.length() && !dontNeedEncoding.get((c = (int) s.charAt(i))));
+                charArrayWriter.flush();
+                String str = new String(charArrayWriter.toCharArray());
+                byte[] ba = str.getBytes(charset);
+                for (int j = 0; j < ba.length; j++) {
+                    out.append('%');
+                    char ch = Character.forDigit((ba[j] >> 4) & 0xF, 16);
+                    if (Character.isLetter(ch)) {
+                        ch -= caseDiff;
+                    }
+                    out.append(ch);
+                    ch = Character.forDigit(ba[j] & 0xF, 16);
+                    if (Character.isLetter(ch)) {
+                        ch -= caseDiff;
+                    }
+                    out.append(ch);
+                }
+                charArrayWriter.reset();
+                needToChange = true;
+            }
+        }
+        return (needToChange? out.toString() : s);
+    }
+}
---
+++ java/AutoIDQrcode.java
@@ -0,0 +1,58 @@
+package com.pepay.app.commons.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.UUID;
+
+/**
+ * Created by aqqje on 2019/9/19 0019
+ */
+public class AutoIDQrcode {
+
+
+    private final static String ALIYPAYROOT = "https://render.alipay.com/p/s/i?scheme=";
+    private final static String ALIPAYSUBROOT= "alipays://platformapi/startapp?saId=10000007&qrcode=";
+    private final static String ALIPAYSUFFIX = "?_s=web-other";
+
+    /**
+     * 自动识别支付宝收款码，并返回连接
+     * @param url
+     * @return
+     */
+    public static String autoIDAlipayQrcode(String url) {
+        String alipayPath = "";
+        // 获取文件后缀
+        String tempFilePath = "";
+        final File excelFile;
+        try {
+            // 通过 url 获取图片
+            tempFilePath = HttpURLConnectionUtil.downloadPicture(url);
+            excelFile = new File(tempFilePath);
+            String qrcodeStr = QRCodeUtil.decode(excelFile).toUpperCase();
+            System.out.println(qrcodeStr);
+            // 短网址处理
+            qrcodeStr = BaiduDwz.createShortUrl(handlerStr(qrcodeStr+ALIPAYSUFFIX));
+            // step one URL encode
+            alipayPath = AURLEncoder.encode(handlerStr(qrcodeStr));
+            // step two URL encode
+            alipayPath =  AURLEncoder.encode(handlerStr(ALIPAYSUBROOT + alipayPath));
+            // step three
+            alipayPath = ALIYPAYROOT + alipayPath;
+            return alipayPath;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            HttpURLConnectionUtil.deleteFile(tempFilePath);
+        }
+        return null;
+    }
+
+    private static String handlerStr(String str){
+        return str.replace("\n", "").replace("\t", "").replace("\r", "");
+    }
+}
